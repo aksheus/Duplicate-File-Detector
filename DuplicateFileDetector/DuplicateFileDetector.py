20342db57bd068365f3fc517d7f7026f7084a33c
akshay kulkarni <kumar.akshay.kulkarni@gmail.com>
2017-05-22 18:52:25 -0500

    removed unecessary code from main

--- DuplicateFileDetector/DuplicateFileDetector.py
+++ DuplicateFileDetector/DuplicateFileDetector.py
@@ -1,37 +1,5 @@
-"""
-traverse directory tree from given node and find all duplicate files and sort the output by the file sizes"""
-import os
-import hashlib
-from collections import defaultdict
-from operator import itemgetter
-from multiprocessing import Pool
 from Detector import Detector
-
-def ComputeFileHashes(f,buffer_size=65536):
-	try:
-		with open(f,'rb') as hash_this:
-			md5_hasher=hashlib.md5()
-			while True:
-				data=hash_this.read(buffer_size)
-				if not data:
-					break
-				md5_hasher.update(data)
-			return (f,md5_hasher.digest())
-	except FileNotFoundError:
-		pass 
-
-def GetDuplicates(dup_dict):
-	for key in dup_dict.keys():
-		if len(dup_dict[key])>2:
-			yield tuple(dup_dict[key])
-
-def GenerateFiles(start):
-	for base,dirs,files in os.walk(start):
-		files_arg=(os.path.join(base,x) for x in files)
-		for fi in files_arg:
-			yield fi 
-
-
+import os 
 if __name__=='__main__':
 	print("enter the path for the starting node in the directory tree: ")
 	start=input() 
